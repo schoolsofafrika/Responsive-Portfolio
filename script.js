050e7a33399087a3203067944bd12a7396fbc012
Echezona Okonkwo <okonkwoaugustechezona@gmail.com>
2022-11-22 17:34:54 +0100

    modified js file

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const navSlide = () => {
-    const menuButton = document.querySelector('.menu-button');
-    const navbarLinks = document.querySelector('.navbar-links');
-
-    menuButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active')
-    })
+const navLink = document.querySelectorAll('.nav-link');
+const menuButton = document.querySelector('.menu-button');
+const navbarLinks = document.querySelector('.navbar-links');
+function navSlide() {
+  navbarLinks.classList.toggle('active');
 }
-navSlide();
+function close() {
+  navbarLinks.classList.remove('active');
+}
+menuButton.addEventListener('click', navSlide);
+navLink.forEach((n) => n.addEventListener('click', close));
